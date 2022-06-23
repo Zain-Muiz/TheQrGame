928cf5b94779938b7577de0926f81d3596238fa6
Zain-Muiz <zainu10863@gmail.com>
2022-06-23 13:08:03 +0530

    Created updated

--- src/db/migrations/20220622052259-create-user-qrs.js
+++ src/db/migrations/20220622052259-create-user-qrs.js
@@ -27,11 +27,9 @@ module.exports = {
         },
       },
       createdAt: {
-        allowNull: false,
         type: Sequelize.DATE,
       },
       updatedAt: {
-        allowNull: false,
         type: Sequelize.DATE,
       },
     });
